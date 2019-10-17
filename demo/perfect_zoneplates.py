ddaf510357ccaae37eb66d163641800680bb0e7a
lreyn <lreyn@umich.edu>
2019-10-17 19:58:31 -0400

    who knows

--- demo/perfect_zoneplates.py
+++ demo/perfect_zoneplates.py
@@ -16,11 +16,11 @@ def sinwave(x):
 
 
 def ripple(x, y, wavelength):
-    return squarewave(math.sqrt(x ** 2 + y ** 2) / wavelength**2)
+    return squarewave(math.sqrt(x ** 2 + y ** 2) / wavelength ** 2)
 
 
-def zoneplate(x, y, wavelength):
-    return squarewave(math.sqrt(x ** 2 + y ** 2) ** 2 / wavelength**2)
+def zoneplate(x, y, wavelength, exp):
+    return squarewave(math.sqrt(x ** 2 + y ** 2) ** exp / wavelength ** 2)
 
 
 def sample(function, a_range, b_range, a_density, b_density):
@@ -40,7 +40,7 @@ def sample_log(function, a_range, b_range, a_density, b_density):
     points = set()
     for a in np.geomspace(a_range[0], a_range[1], num=a_length * a_density):
         for b in np.geomspace(b_range[0], b_range[1], num=b_length * b_density):
-            #print(a, b)
+            # print(a, b)
             points.add((a, b, function(a, b)))
 
     return points
@@ -58,31 +58,35 @@ def make_ripple(x_range, y_range, x_density, y_density, wavelength):
     return canv
 
 
-def make_zoneplate(x_range, y_range, x_density, y_density, wavelength):
-    pts = sample(lambda a, b: zoneplate(a, b, wavelength), x_range, y_range, x_density, y_density)
+def make_zoneplate(x_range, y_range, x_density, y_density, wavelength, resolution, exp):
+    pts = sample(lambda a, b: zoneplate(a, b, wavelength, exp), x_range, y_range, x_density, y_density)
     canv = hl.set_to_gradient(pts,
                               x_range=x_range,
                               y_range=y_range,
                               black_ref=-1,
                               white_ref=1,
                               default=hl.GRAY,
-                              resolution=50)
+                              resolution=resolution)
     return canv
 
 
-x_range = (0, 1)
-y_range = (0, 1)
-x_density = 20
-y_density = 20
+x_range = (-20, 20)
+y_range = (-20, 20)
+x_density = 10
+y_density = 10
 wavelength = 1
+resolution = 30
+exp = 2.5
 
-canv = make_zoneplate(x_range, y_range, x_density, y_density, wavelength)
+canv = make_zoneplate(x_range, y_range, x_density, y_density, wavelength, resolution, exp)
 hl.render_from_array(canv)
-hl.save_img(canv, 'zoneplates/discrete-zoneplate_xd{2}-yd{3}-w{4}'.format(x_range,
-                                                                           y_range,
-                                                                           x_density,
-                                                                           y_density,
-                                                                           wavelength))
+hl.save_img(canv, 'zoneplates/discrete-zoneplate_xd{2}-yd{3}-w{4}-r{5}-exp{6}'.format(x_range,
+                                                                                      y_range,
+                                                                                      x_density,
+                                                                                      y_density,
+                                                                                      wavelength,
+                                                                                      resolution,
+                                                                                      exp))
 
 '''hl.video(frame_func=lambda t: make_zoneplate(x_range=x_range,
                                              y_range=y_range,
