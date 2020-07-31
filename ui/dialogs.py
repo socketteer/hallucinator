16d28aaa5d890aed7b9d2b48cb4d4986d8dce9ca
Kyle McDonell <kylemcdonell@gmail.com>
2020-07-31 06:02:31 -0400

    Fixes UI object controls, adds object create, delete, render. Lots of ugly code

---
+++ ui/dialogs.py
@@ -0,0 +1,108 @@
+# coding=utf-8
+# dialogs.py
+#
+# Dialog boxes for display.py
+# Written by Kyle McDonell
+#
+# CS 251
+# Spring 2016
+
+import tkinter as tk
+from tkinter import ttk, font, filedialog
+import csv
+from ui import controls
+from ui.deprecated import wizards
+
+# Class to create a basic dialog pop-up box.  Designed for extension.
+# From http://effbot.org/tkinterbook/tkinter-dialog-windows.htm
+class Dialog(tk.Toplevel):
+    def __init__(self, parent, title=None):
+        tk.Toplevel.__init__(self, parent)
+        self.transient(parent)
+        self.wm_resizable(height=False, width=False)
+
+        if title:
+            self.title(title)
+
+        self.parent = parent
+        self.result = None
+        body = tk.Frame(self)
+        self.initial_focus = self.body(body)
+        body.pack(padx=5, pady=5)
+
+        self.buttonbox()
+        self.grab_set()
+        if not self.initial_focus:
+            self.initial_focus = self
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+        self.geometry("+%d+%d" % (parent.winfo_rootx() + 50,
+                                  parent.winfo_rooty() + 50))
+        self.initial_focus.focus_set()
+
+        self.wait_window(self)
+
+    # construction hooks
+    def body(self, master):
+        # create dialog body.  return widget that should have
+        # initial focus.  this method should be overridden
+        pass
+
+    def buttonbox(self):
+        # add standard button box. override if you don't want the standard buttons
+        box = tk.Frame(self)
+
+        w = tk.Button(box, text="OK", width=10, command=self.ok, default=tk.ACTIVE)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+        w = tk.Button(box, text="Cancel", width=10, command=self.cancel)
+        w.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+        box.pack()
+
+    # standard button semantics
+    def ok(self, event=None):
+        if not self.validate():
+            self.initial_focus.focus_set()  # put focus back
+            return
+        self.withdraw()
+        self.update_idletasks()
+        self.apply()
+        self.cancel()
+
+    def cancel(self, event=None):
+        # put focus back to the parent window
+        self.parent.focus_set()
+        self.destroy()
+
+    # command hooks
+    def validate(self):
+        return 1  # override
+
+    def apply(self):
+        pass  # override
+
+
+class SelectorDialog(Dialog):
+
+    def __init__(self, parent, title, choices, callback):
+        self.choices = choices
+        self.callback = callback
+        print("Did I do it?")
+        super().__init__(parent, title)
+
+    def body(self, master):
+        # Build the listbox
+        height = min(15, max(4, len(self.choices)))
+        self.listBox = tk.Listbox(master, width=40, height=height, selectmode="SINGLE")
+        for choice in self.choices:
+            self.listBox.insert(tk.END, choice)
+        self.listBox.grid(row=1, columnspan=4)
+
+    def validate(self):
+        return len(self.listBox.curselection()) > 0
+
+    def apply(self):
+        for idx in self.listBox.curselection():
+            self.callback(self.choices[idx])
+
--- ui/display.py
+++ ui/display.py
@@ -6,7 +6,9 @@
 #
 # CS 251
 # Spring 2016
+
 import tkinter as tk
+import uuid
 from dataclasses import asdict
 from pprint import pprint
 
@@ -15,26 +17,25 @@ import PIL.ImageTk
 import math
 import hallucinator as hl
 
-
-from ui import controls, objects
+from ui import controls, objects, dialogs
 from ui.objects import ViewSettings, ColorStyle, PlotStyle, ComputedObject
 
 
-class DisplayTab(object):
+class DisplayTab:
 
     # Initializes the display
     def __init__(self, notebook, root):
+        # Parents
         self.root = root
         self.notebook = notebook
-        self.frame = tk.Frame(notebook)
 
         # Data
-        self.view_settings, self.objects = self.default_settings()
-        self.info_label = tk.StringVar(value="")
+        self.view_settings, default_objects = self.default_settings()
+        self.objects = {uuid.uuid1(): obj for obj in default_objects}
 
-        # Build the parts of the application
+        # Build the parts of the tab
+        self.frame = tk.Frame(notebook)
         self.build_controls()
-        self.build_info_bar()
         self.build_canvas()
         self.set_mouse_bindings()
 
@@ -47,116 +48,161 @@ class DisplayTab(object):
         return view_settings, objs
 
     #################################
-    #       Build components
+    #       Controls # FIXME This area will never be beautiful :(
     #################################
 
-    # Build a control panel for the user
     def build_controls(self):
-        # Create a separator and a control frame on the right side of the window
+        # FIXME setting class variables in each function. Complicated, but stateless is harder.
+        #  Would need refresh functions. Probably needs to be rethought...
+        self.build_control_frame()
+        self.build_control_panel()
+
+        controls.create_separator(self.control_panel)
+        self.build_view_controls()
+        controls.create_separator(self.control_panel)
+        self.build_object_selector()
+
+        self.build_object_controls()
+        self.build_rendering_controls()
+
+    # Create a separator and a control frame on the right side of the window
+    def build_control_frame(self):
         self.control_frame = tk.Frame(self.frame)
         self.control_frame.pack(side=tk.RIGHT, padx=2, pady=2, fill=tk.Y)
+        # Separator between the controls and the main window
         sep = tk.Frame(self.frame, height=3000, width=2, bd=1, relief=tk.SUNKEN)
         sep.pack(side=tk.RIGHT, padx=2, pady=2, fill=tk.Y)
 
+    # Build a titled frame for controls to be put in
+    def build_control_panel(self):
         self.control_panel = tk.Frame(self.control_frame)
         self.control_panel.pack(side=tk.TOP, fill=tk.Y)
-
-        # Separator between the controls and the main window
         controls.create_title(self.control_panel, "Control Panel")
 
-        # Build View Controls
-        controls.create_separator(self.control_panel)
+    def build_view_controls(self):
         controls.create_header(self.control_panel, "View Settings")
-        self.view_setting_controls = controls.build_controls_for_dataclass(
+        self.view_controls = controls.build_controls_for_dataclass(
             self.control_panel, self.view_settings, self.autorender
         )
 
-        # Object selector
-        controls.create_separator(self.control_panel)
-        self.object_selector_row = self.control_panel.grid_size()[1]
-        self.object_selector = self.build_object_selector()
-
-        # Object controls
-        self.build_object_controls()
-
-        # Rendering controls # TODO make a grid, split into section s
-        self.rendering_controls = tk.Frame(self.control_frame, width=2, bd=1, relief=tk.SUNKEN)
-        self.rendering_controls.pack(side=tk.RIGHT, padx=2, pady=2, fill=tk.X)
-        button = controls.create_button(self.rendering_controls, "Render", lambda: self.render)
-        button.pack()
-        button = controls.create_button(self.rendering_controls, "Delete", lambda: self.objects.pop(self.object_selector.current_index))
-        button.pack()
-        # rendering_controls.pack(side=tk.BOTTOM, padx=2, pady=2, fill=tk.Y)
-
-
     def build_object_selector(self):
-        values = [obj.name for obj in self.objects]
+        values = [obj.name for obj_id, obj in self.objects.items()]
 
-        if not hasattr(self, "selected_object_string"):
+        # On first run, record the row so the selector can be rebuilt in the same place
+        if not hasattr(self, "object_selector_row"):
+            self.object_selector_row = self.control_panel.grid_size()[1]
             self.selected_object_string = tk.StringVar()
             self.selected_object_string.set(values[0])
             self.selected_object_string.trace_add("write", lambda *_: self.build_object_controls())
 
-        label, control = controls.create_combo_box(
+        # Set selected obj to the first object name if invalid
+        # FIXME There should be a function to select an object, not just done implicitly
+        if self.selected_object_string.get() not in values:
+            self.selected_object_string.set(values[0] if values else "")
+
+        label, self.object_selector = controls.create_combo_box(
             self.control_panel, text="Object", row=self.object_selector_row,
             variable=self.selected_object_string, values=values
         )
-        return control
 
+    # Build object controls for all objects at the current row of the control_panel if they don't exist and then hide them
+    # FIXME I don't like the way this is done. This should probably be multiple functions (create, update, ...?)
+    # TODO Scroll bar
     def build_object_controls(self):
-        if hasattr(self, "object_controls"):  # FIXME I should probably just define everything in init...
-            def destroyer(x):
-                if hasattr(x, "destroy"):
-                    x.destroy()
-            hl.recursive_map(destroyer, self.object_controls)
+        # On first run, create the object_controls dict uuid->controls
+        if not hasattr(self, "object_controls"):
+            self.object_control_frame = tk.Frame(self.control_frame)
+            self.object_control_frame.pack(side=tk.TOP, fill=tk.Y)
+            self.object_controls = {}
 
-        selected_object = self.objects[self.object_selector.current()]
-        self.object_controls = controls.build_controls_for_dataclass(
-            self.control_panel, selected_object.params, self.autorender
-        )
+        # Hide all existing object controls
+        for c in self.object_control_frame.children.values():
+            c.grid_remove()
 
+        # Create controls for any object that doesn't have them and hide them
+        for obj_id, obj in self.objects.items():
+            if obj_id not in self.object_controls:
+                self.object_controls[obj_id] = controls.build_controls_for_dataclass(
+                    self.object_control_frame, obj.params, self.autorender
+                )
+                for c in self.object_control_frame.children.values():
+                    c.grid_remove()
 
-        # self.param_defaults, param_types = get_param_info(ViewSettings)
-        # for i, (param_name, param_default) in enumerate(self.param_defaults.items()):
-        #     print(param_name, param_types[param_name])
-        #     var, control = build_control_row_for_parameter(
-        #         cFrame,
-        #         row=i+2,
-        #         param_name=param_name,
-        #         param_default=param_default,
-        #         param_type=param_types[param_name],
-        #         param_dict=self.param_defaults)
-        #     self.x.append((var, control))
+        # Show active object controls
+        if self.current_object:
+            hl.recursive_map(lambda con: con.grid(), self.object_controls[self.current_object[1]])
+        self.autorender()
 
-    # Build an info bar below the canvas to display info variables
-    def build_info_bar(self):
-        bar = tk.Frame(self.frame)
-        bar.pack(side=tk.BOTTOM, padx=2, pady=2, fill=tk.X)
-        # make a separator frame
-        sep = tk.Frame(self.frame, width=2000, height=2, bd=1, relief=tk.SUNKEN)
-        sep.pack(side=tk.BOTTOM, padx=2, pady=2, fill=tk.Y)
-        # create a label to display info on the canvas
-        tk.Label(bar, textvariable=self.info_label).pack(side=tk.LEFT)
-        self.updateInfoBar()
+    # Build a control panel for the user
+    def build_rendering_controls(self):
+        self.rendering_controls = tk.Frame(self.control_frame, width=2, bd=1)
+        self.rendering_controls.pack(side=tk.BOTTOM, fill=tk.X)
+        self.rendering_controls.grid_columnconfigure(0, weight=1)
+        self.rendering_controls.grid_columnconfigure(1, weight=1)
 
-    # Build the canvas on which data is drawn
-    def build_canvas(self):
-        self.canvas = tk.Canvas(self.frame, width=500, height=500)
-        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+        button = tk.Button(self.rendering_controls, text="Create Object", command=self.open_create_object_dialog)
+        button.grid(row=0, column=0, sticky="ew")
+
+        button = tk.Button(self.rendering_controls, text="Delete", command=self.delete_current_object)
+        button.grid(row=0, column=1, sticky="ew")
+
+        button = tk.Button(self.rendering_controls, text="Render", command=self.render)
+        button.grid(row=1, column=0, columnspan=2, sticky="ew")
+        # rendering_controls.pack(side=tk.BOTTOM, padx=2, pady=2, fill=tk.Y)
 
     # TODO Set up user bindings
     def set_mouse_bindings(self):
         pass
 
     #########################################
-    #      Build and Update Visuals
+    #   Dialogs
     #########################################
 
+    # Create a dialog box to adjust mouse sensitivity for viewing control
+    def open_create_object_dialog(self):
+        dialogs.SelectorDialog(self.root, "Create Object",
+                               choices=list(objects.available_objects.keys()),
+                               callback=self.create_object)
 
+    #########################################
+    #   Util
+    #########################################
+
+    # Returns a tuple (object, uuid) FIXME This is a dangerous way to do this. Asking for a stack overflow...
+    @property
+    def current_object(self):
+        self.build_object_selector()
+        object_list = list(self.objects.items())
+        return object_list[self.object_selector.current()][::-1] if len(self.objects.items()) > 0 else None
+
+    def create_object(self, obj_name):
+        obj = ComputedObject.new(name=obj_name, func=objects.available_objects[obj_name])
+        self.objects[uuid.uuid1()] = obj
+        self.refresh()
+
+    def delete_current_object(self):
+        obj, obj_id = self.current_object
+        self.objects.pop(obj_id)
+        self.object_controls.pop(obj_id)
+        self.refresh()
+
+    def refresh(self):
+        self.build_object_selector()
+        self.build_object_controls()
+        self.autorender()
+
+    #########################################
+    #   Visuals
+    #########################################
+
+    # Build the canvas on which data is drawn
+    def build_canvas(self):
+        self.canvas = tk.Canvas(self.frame, width=500, height=500)
+        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
 
     def autorender(self, *args):
-        if self.view_settings.autorender:
-            self.render()
+        if self.view_settings.autorender and hasattr(self, "canvas"):  # FIXME Ugh. This is called before the canvas is built
+            self.render(*args)
 
     # TODO Optimize - imagify all at once, make style flexible
     def render(self, *args):
@@ -165,33 +211,22 @@ class DisplayTab(object):
             # f = hl.contour(f, threshold=2*math.pi)
             # return hl.imagify(f, bwref=[0, 2*math.pi], hsv=self.view_settings.style == ColorStyle.HSV)
 
-        # It's objects job to return an image
+            # It's objects job to return an image
             return f
 
-
-        if self.view_settings.render_all and len(self.objects) > 1:
-            object_images = [obj_to_image(obj) for obj in self.objects]
-            image = hl.tile_images(object_images)
-        else:
-            image = obj_to_image(self.objects[self.object_selector.current()])
-        image = hl.np.swapaxes(image, 0, 1)
-        tk_image = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(image))
-
-
-        # Save the image or python will garbage collect, even if tk is displaying it...
         self.canvas.delete("image")
-        self.image = tk_image
-        self.canvas.create_image(0, 0, image=self.image, anchor=tk.NW, tag="image")
-        self.updateInfoBar()
+        if len(self.objects) > 0:
+            if self.view_settings.render_all and len(self.objects) > 1:
+                object_images = [obj_to_image(obj) for obj_id, obj in self.objects.items()]
+                image = hl.tile_images(object_images)  # TODO This function sucks
+            else:
+                image = obj_to_image(self.current_object[0])
+            image = hl.np.swapaxes(image, 0, 1)
+            tk_image = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(image))
 
-    def update_controls(self):
-        self.build_object_selector()
-        self.build_object_controls()
-
-    # Update the info bar below the canvas
-    def updateInfoBar(self):
-        self.info_label.set("I can put an info string here")
-
+            # Save the image or python will garbage collect, even if tk is displaying it...
+            self.image = tk_image
+            self.canvas.create_image(0, 0, image=self.image, anchor=tk.NW, tag="image")
 
     #################################
     #      File I/O actions TODO
@@ -209,7 +244,6 @@ class DisplayTab(object):
     #         self.regressionSettings = {}
     #         self.analysisList = []
     #         self.rawData = ...
-    #         self.updateInfoBar()
     #         self.notebook.tab(self.frame, text=filename.split('/')[-1])
     #
     # # Save the opened data set to a csv file
@@ -220,7 +254,3 @@ class DisplayTab(object):
     #     file = filedialog.asksaveasfilename(defaultextension='.csv')
     #     if file:
     #         self.rawData.save_csv(file)
-
-
-
-
